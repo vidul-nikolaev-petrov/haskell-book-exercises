db8bc42cc166b53068c7afeac6683407e0fa1c8a
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2023-06-14 14:00:31 +0300

    some exercises

---
+++ charpter-11/capitalize.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Data.Char (isLower, toUpper)
+
+capitalizeWord :: String -> String
+capitalizeWord [] = []
+capitalizeWord t@(x : xs)
+  | isLower x = toUpper x : xs
+  | otherwise = t
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph = capitalizeWord . unwords . capitalize . words
+  where
+    capitalize [] = []
+    capitalize [x] = [x]
+    capitalize (x : y : xs)
+      | last x == '.' = x : capitalizeWord y : capitalize xs
+      | last y == '.' = x : capitalize (y : xs)
+      | otherwise = x : y : capitalize xs
+
+main :: IO ()
+main = do
+  print $ capitalizeWord "Tester"
+  print $ capitalizeWord "Tester"
+  print $ capitalizeParagraph "bla. new blue sky. dom el."
---
+++ charpter-11/hutton-razor.hs
@@ -0,0 +1,35 @@
+module HuttonRazor where
+
+import Language.Haskell.TH (Exp)
+
+data Expr
+  = Lit Integer
+  | Add Expr Expr
+
+eval :: Expr -> Integer
+eval (Lit int) = int
+eval (Add x y) = eval x + eval y
+
+exp1 :: Expr
+exp1 = Lit 10
+
+exp2 :: Expr
+exp2 = Lit 20
+
+exp3 :: Expr
+exp3 = Add exp1 exp2
+
+exp4 :: Expr
+exp4 = Add exp3 (Lit 100)
+
+printExpr :: Expr -> String
+printExpr (Lit int) = show int
+printExpr (Add x y) = printExpr x ++ " + " ++ printExpr y
+
+main :: IO ()
+main = do
+  print $ eval exp1
+  print $ eval exp4
+  print $ eval (Add (Lit 1) (Lit 9001))
+  print $ printExpr (Add (Lit 1) (Lit 9001))
+  print $ printExpr exp4
