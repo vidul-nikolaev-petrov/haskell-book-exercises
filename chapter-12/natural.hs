768b7559c4755da9ab87ff16a75089510fa1c9a1
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2023-06-28 13:36:51 +0300

    chapter 12 - integerToNat

--- chapter-12/natural.hs
+++ chapter-12/natural.hs
@@ -6,8 +6,19 @@ natToInteger :: Nat -> Integer
 natToInteger Zero = 0
 natToInteger (Succ x) = 1 + natToInteger x
 
+integerToNat :: Integer -> Maybe Nat
+integerToNat x | x < 0 = Nothing
+integerToNat x = Just $ toNat x
+  where
+    toNat x
+      | x == 0 = Zero
+      | otherwise = Succ (toNat (x - 1))
+
 main :: IO ()
 main = do
   print $ natToInteger Zero
   print $ natToInteger (Succ Zero)
   print $ natToInteger (Succ (Succ Zero))
+  print $ integerToNat (-1)
+  print $ integerToNat 0
+  print $ integerToNat 8
